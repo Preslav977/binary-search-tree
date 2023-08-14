8524d842e0c87f1739d84918261727cbf606aaed
Preslav977 <p.cvetanow@gmail.com>
2023-08-14 13:46:49 +0300

    buildTree function creates balanced tree is now working

--- src/Tree.js
+++ src/Tree.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-underscore-dangle */
+import Node from "./Node";
+
 class Tree {
   array = [];
 
@@ -11,6 +14,29 @@ class Tree {
 
   set root(value) {
     this._root = value;
+  }
+
+  buildTree(array, start = 0, end = array.length - 1) {
+    const newArray = [];
+
+    array.sort((smallerValue, biggerValue) => smallerValue - biggerValue);
+    array.forEach((element) => {
+      if (!newArray.includes(element)) {
+        newArray.push(element);
+      }
+    });
+
+    if (start > end) {
+      return null;
+    }
+    const middle = Math.floor(parseInt(start + end) / 2);
+    const newNode = new Node(newArray[middle]);
+
+    newNode.leftChildren = this.buildTree(array, start, middle - 1);
+
+    newNode.rightChildren = this.buildTree(array, middle + 1, end);
+
+    return newNode;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import Node from "./Node";
 import Tree from "./Tree";
 import "./style.css";
 
-const newNode = new Node(5);
+const newTree = new Tree(5);
 
-console.log(newNode);
-
-const newTree = new Tree(1);
-
-console.log(newTree);
+console.log(
+  newTree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+);
