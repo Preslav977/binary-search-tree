e672dc72945b897ca53f62983c64f2821ad360e7
Preslav977 <p.cvetanow@gmail.com>
2023-09-04 09:14:24 +0300

    Created small script to test all the methods

--- src/index.js
+++ src/index.js
@@ -3,27 +3,11 @@ import "./style.css";
 
 const balancedTree = new Tree();
 
-// console.log(balancedTree.buildTree([0, 6, 1, 2, 5, 3, 7, 8, 9, 6], 0, 8));
+console.log(balancedTree.createRandomNumbers(0, 4));
 
-// balancedTree.prettyPrint();
+balancedTree.prettyPrint();
 
-// console.log(balancedTree.insert(4));
-
-// balancedTree.prettyPrint();
-
-// console.log(balancedTree.insert(10));
-
-// console.log(balancedTree.insert(20));
-
-// console.log(balancedTree.insert(30));
-
-// balancedTree.prettyPrint();
-
-// console.log(balancedTree.delete(1));
-
-// balancedTree.prettyPrint();
-
-// console.log(balancedTree.find(8));
+console.log(balancedTree.isBalanced());
 
 // console.log(balancedTree.levelOrder());
 
@@ -33,16 +17,40 @@ const balancedTree = new Tree();
 
 // console.log(balancedTree.postOrder());
 
-// console.log(balancedTree.height(balancedTree.find(6)));
+console.log(balancedTree.insert(102));
 
-// console.log(balancedTree.depth());
+console.log(balancedTree.insert(110));
 
-// console.log(balancedTree.isBalanced());
+console.log(balancedTree.insert(120));
 
-// console.log(balancedTree.reBalance(0, 12));
+balancedTree.prettyPrint();
+
+console.log(balancedTree.isBalanced());
+
+console.log(balancedTree.reBalance(0, 12));
+
+balancedTree.prettyPrint();
+
+console.log(balancedTree.isBalanced());
+
+// console.log(balancedTree.levelOrder());
+
+// console.log(balancedTree.inOrder());
+
+// console.log(balancedTree.preOrder());
+
+// console.log(balancedTree.postOrder());
+
+console.log(balancedTree.insert(20));
+
+balancedTree.prettyPrint();
+
+// console.log(balancedTree.delete(20));
 
 // balancedTree.prettyPrint();
 
-console.log(balancedTree.createRandomNumbers(0, 4));
+console.log(balancedTree.find(20));
 
-balancedTree.prettyPrint();
+console.log(balancedTree.height(balancedTree.find(20)));
+
+// console.log(balancedTree.depth(new Node(20)));
